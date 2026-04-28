7b9181ae6c8e051a437bbc4cbc3936e53f559ad5
Yevgeniy <jekap99@mail.ru>
2026-04-28 13:13:25 +0500

    add db

--- database/db.py
+++ database/db.py
@@ -6,6 +6,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
+
+DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+
 engine = create_async_engine(DATABASE_URL, echo=False)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
